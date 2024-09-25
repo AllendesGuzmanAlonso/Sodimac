cd3d898ce199df140aa0bcd2cb27c5b7bee457d6
AllendesGuzmanAlonso <capstone412@gmail.com>
2024-09-25 16:17:01 -0300

    avance duoc uc

--- app/src/routes/main_routes.py
+++ app/src/routes/main_routes.py
@@ -2,26 +2,20 @@ from flask import Blueprint, render_template, request
 
 main_bp = Blueprint('main_bp', __name__)
 
+
+# Ruta de login aceptando tanto GET como POST
 @main_bp.route('/')
-def index():
-    return render_template('index.html')
+def login():
+    return render_template('login.html')
 
 @main_bp.route('/home')
 def home():
     return render_template('home.html')
 
-# Ruta de login aceptando tanto GET como POST
-@main_bp.route('/login')
-def login():
-    return render_template('login.html')
 
-@main_bp.route('/login2')
-def login2():
-    return render_template('login2.html')
-
-@main_bp.route('/registro-herramientas')
+@main_bp.route('/registroHerramientas')
 def registroHerramientas():
-    return render_template('registro-herramientas.html')
+    return render_template('registroHerramientas.html')
 
 @main_bp.route('/transacciones')
 def transacciones():
